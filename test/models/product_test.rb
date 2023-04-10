896fbaef46d82c161e3e2ceec26481d23d771796
Murat Göksel <hmgoksel@gmail.com>
2023-04-10 12:23:16 -0400

    Validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,61 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'product attributes must not be empty' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    p = Product.new(title: 'My Book Title', description: 'yyy', 
+                    image_url: 'zzz.jpg')
+    p.price = -1
+    assert p.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], p.errors[:price]
+
+    p.price = 0
+    assert p.invalid?
+    assert_equal ['must be greater than or equal to 0.01'], p.errors[:price]
+
+    p.price = 1
+    assert p.valid?
+  end
+
+  test 'image url' do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+      http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |image_url|
+      assert new_product(image_url).valid?, "#{image_url} should be valid"
+    end
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?, "#{image_url} should not be valid"
+    end
+  end
+
+  test 'product is not valud without a unique title' do
+    p = Product.new(title: products(:ruby).title, description: 'yyy', price: 1,
+                    image_url: 'fred.gif')
+    assert p.invalid?
+    assert_equal ['has already been taken'], p.errors[:title]
+  end
+
+  test 'product is not valud without a unique title - i18n' do
+    p = Product.new(title: products(:ruby).title, description: 'yyy', price: 1,
+                    image_url: 'fred.gif')
+    assert p.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')], p.errors[:title]
+  end
+
+  def new_product(image_url)
+    p = Product.new(title: 'My Book Title', description: 'yyy', price: 1,
+                    image_url: image_url)
+  end
+
 end
