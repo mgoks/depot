5a8dc1a64a20ee7d98767d8a1528d2b0e2c26203
Murat Göksel <hmgoksel@gmail.com>
2023-04-10 12:31:17 -0400

    Validate title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {
+    minimum: 10,
+    too_short: "title has to be at least %{count} characters long"
+  }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG, or PNG image.'
