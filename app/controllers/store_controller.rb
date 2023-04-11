f05124ec0998f64e745153d539c42ddc47601698
Murat Göksel <hmgoksel@gmail.com>
2023-04-11 15:22:40 -0400

    Create cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
   def index
+    record_index_access
     @products = Product.order(:title)
   end
+
+  private
+    def record_index_access
+      if session[:index_access_counter].nil?
+        session[:index_access_counter] = 1
+      else
+        session[:index_access_counter] += 1
+      end
+    end
 end
